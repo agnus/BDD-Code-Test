baa23d505d99ee106807aba82f1a06fe4da3912f
Agnus <opensource@agnusvietnam.com>
2010-03-20 23:52:12 -0400

    adding feature / step defs

---
+++ features/step_definitions/gherkin_steps.rb
@@ -0,0 +1,8 @@
+Given /^there is feature titled "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the feature has "([^\"]*)" of "([^\"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
