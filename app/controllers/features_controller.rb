1e9f8d5f1c730c3d4da9e1b0a637b08741423ea1
Agnus <opensource@agnusvietnam.com>
2010-03-21 22:48:17 -0400

    adding feature controller and spec

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,2 +1,5 @@
 class FeaturesController < ApplicationController
+  def index
+    @features = Feature.all
+  end
 end
--- spec/controllers/features_controller_spec.rb
+++ spec/controllers/features_controller_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 
 describe FeaturesController do
-
-  #Delete this example and add some real ones
-  it "should use FeaturesController" do
-    controller.should be_an_instance_of(FeaturesController)
+  context "#index" do
+    it "should provide an index method that returns all features to the view" do
+      @features = Array.new(3) { Factory(:feature) }
+      Feature.stub!(:find).and_return(@features)
+      get :index
+      assigns[:features].should == @features
+    end
+    it "should provide an index method that returns an empty array if there are no features" do
+      get :index
+      assigns[:features].empty?.should be(true)
+    end
+    it "should render the index template" do
+      get :index
+      response.should render_template(:index)
+    end
   end
-
 end
