b24a1c4806c0af4fd37dcf5e7ce10803adbc93fa
endax <endax@khanh.(none)>
2011-03-07 15:57:03 +0700

    Upgrade Rails 2 App to Rails 3

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,3 +1,4 @@
 class Feature < ActiveRecord::Base
+  has_many :scenarios
   validates_presence_of :title
 end
--- features/step_definitions/gherkin_steps.rb
+++ features/step_definitions/gherkin_steps.rb
@@ -1,8 +1,9 @@
 Given /^there is feature titled "([^\"]*)"$/ do |title|
-  feature = Factory(:feature)
+  @feature = Factory(:feature)
 end
 
 Given /^the feature has "([^\"]*)" of "([^\"]*)"$/ do |attribute, value|
-  pending # express the regexp above with the code you wish you had
+ if attribute != "feature_title"
+  scenario = Factory(:scenario,:feature_id => @feature.id,attribute=>value)
+ end
 end
-
